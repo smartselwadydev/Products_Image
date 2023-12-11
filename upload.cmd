211992de71067d9289278382b73fb168903affe7
unknown <media@smartselwady.com>
2023-12-11 15:01:43 +0300

    first commit

--- upload.cmd
+++ upload.cmd
@@ -1,7 +1,6 @@
-git init
-git add README.md
+git status
 git add .
 git commit -m "first commit"
-git branch -M main
 git remote add origin https://github.com/smartselwadydev/Products_Image.git
+git branch -M main
 git push -u origin main
